05f6908f890011e8acdd26fcc8cd764e50f29f6a
Jared  E <jared.epps322@gmail.com>
2020-04-25 22:07:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,7 +7,11 @@ def my_hash
 
 
 def shipping_manifest
- shipping_manifest = 
+ shipping_manifest =  {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
 
 
 end
